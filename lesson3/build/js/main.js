4388235354012a14e2962534e084cb96fd43b3b8
TonmoyZohani <tonmoyzohani@gmail.com>
2024-07-26 23:23:09 +0600

    objects start

--- lesson3/build/js/main.js
+++ lesson3/build/js/main.js
@@ -19,3 +19,23 @@ console.log(bands);
 let myTuple = ["Dave", 42, true];
 let mixed = ["John", 1, false];
 myTuple[1] = 42;
+// Objects
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+const exampleObj = {
+    prop1: 'Dave',
+    prop2: true,
+};
+exampleObj.prop1 = 'John';
+let evh = {
+    name: 'Eddie',
+    active: false,
+    albums: [1984, 5150, 'OU812']
+};
+let jp = {
+    active: true,
+    albums: ['I', 'II', 'IV']
+};
--- lesson3/src/main.ts
+++ lesson3/src/main.ts
@@ -26,3 +26,34 @@ let myTuple: [string, number, boolean] = ["Dave", 42, true];
 let mixed = ["John", 1, false];
 
 myTuple[1] = 42;
+
+// Objects
+let myObj: object
+myObj = []
+console.log(typeof myObj)
+myObj = bands
+myObj = {}
+
+const exampleObj = {
+    prop1: 'Dave',
+    prop2: true,
+}
+
+exampleObj.prop1 = 'John'
+
+interface Guitarist {
+    name?: string,
+    active: boolean,
+    albums: (string | number)[]
+}
+
+let evh: Guitarist = {
+    name: 'Eddie',
+    active: false,
+    albums: [1984, 5150, 'OU812']
+}
+
+let jp: Guitarist = {
+    active: true,
+    albums: ['I', 'II', 'IV']
+}
