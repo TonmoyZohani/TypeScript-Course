cb0a209540556c8a677dbba83b03783e6ce75f34
TonmoyZohani <tonmoyzohani@gmail.com>
2024-07-26 20:40:08 +0600

    type lesson fixed

--- lesson2/build/js/main.js
+++ lesson2/build/js/main.js
@@ -1,9 +1,23 @@
 "use strict";
-let myName = 'Dave';
+// let myName: string = 'Dave'
+// let meaningOfLife: number;
+// let isLoading: boolean;
+// let album: any;
+// myName = 'John'
+// meaningOfLife = 42
+// isLoading = true
+// album = 5150
+// const sum = (a: number, b: string) => {
+//     return a + b
+// }
+// let postId: string | number
+// let isActive: number | boolean
+// let re: RegExp = /\w+/g
+let myName = "Deve";
 let meaningOfLife;
 let isLoading;
 let album;
-myName = 'John';
+myName = "John";
 meaningOfLife = 42;
 isLoading = true;
 album = 5150;
