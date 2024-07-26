f95f3baa4921c51ab3b8ddd868a6c65e5cc20691
TonmoyZohani <tonmoyzohani@gmail.com>
2024-07-26 22:53:42 +0600

    array complete

---
+++ lesson3/build/js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+let stringArr = ["one", "hey", "Dave"];
+let guitars = ["Strat", "Les Paul", 5150];
+let mixedData = ["EVH", 1984, true];
+stringArr[0] = "John";
+stringArr.push("hey");
+guitars[0] = 1984;
+guitars.unshift("Jim");
+let test = [];
+// let bands: string[] = []
+let bands = [];
+bands.push("Van Halen");
+console.log(bands);
+bands[1] = 'Artcell';
+console.log(bands);
+bands.push("Miles");
+console.log(bands);
+// Tuple
+let myTuple = ["Dave", 42, true];
+let mixed = ["John", 1, false];
+myTuple[1] = 42;
---
+++ lesson3/src/main.ts
@@ -0,0 +1,28 @@
+let stringArr = ["one", "hey", "Dave"];
+
+let guitars = ["Strat", "Les Paul", 5150];
+
+let mixedData = ["EVH", 1984, true];
+
+stringArr[0] = "John";
+stringArr.push("hey");
+
+guitars[0] = 1984;
+guitars.unshift("Jim");
+
+let test = [];
+// let bands: string[] = []
+let bands: string[] = [];
+bands.push("Van Halen");
+console.log(bands);
+bands[1]='Artcell';
+console.log(bands);
+bands.push("Miles");
+console.log(bands);
+
+// Tuple
+let myTuple: [string, number, boolean] = ["Dave", 42, true];
+
+let mixed = ["John", 1, false];
+
+myTuple[1] = 42;
