68d52a3d02e3f6089a518c80a60bc94aafbe103a
TonmoyZohani <tonmoyzohani@gmail.com>
2024-07-28 21:07:09 +0600

    functional operational params

--- lesson4/build/js/main.js
+++ lesson4/build/js/main.js
@@ -16,7 +16,19 @@ const subTract = function (num1, num2) {
 const multiply = (num1, num2) => {
     return num1 * num2;
 };
+// optional parameters
+const addAll = (num1, num2, num3) => {
+    if (num3 !== undefined) {
+        return num1 + num2 + num3;
+    }
+    return num1 + num2;
+};
+const sumAll = (num1, num2, num3) => {
+    return num1 + num2 + num3;
+};
 console.log(add(5, 6));
 messagePrint("This is TypeScript Function");
 console.log(subTract(11, 6));
 console.log(multiply(3, 8));
+console.log(sumAll(7, 9, 8));
+console.log(addAll(5, 9));
--- lesson4/src/main.ts
+++ lesson4/src/main.ts
@@ -43,7 +43,23 @@ const multiply: mathFunction = (num1, num2) => {
   return num1 * num2;
 };
 
+// optional parameters
+
+const addAll = (num1: number, num2: number, num3?: number): number => {
+  if (num3 !== undefined) {
+    return num1 + num2 + num3;
+  }
+
+  return num1 + num2;
+};
+
+const sumAll = (num1: number, num2: number, num3: number): number => {
+  return num1 + num2 + num3;
+};
+
 console.log(add(5, 6));
 messagePrint("This is TypeScript Function");
 console.log(subTract(11, 6));
 console.log(multiply(3, 8));
+console.log(sumAll(7, 9, 8));
+console.log(addAll(5, 9));
