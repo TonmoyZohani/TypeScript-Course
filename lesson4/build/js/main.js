1f535bd39652fd62c31392a307f644591931f127
TonmoyZohani <tonmoyzohani@gmail.com>
2024-07-28 20:06:13 +0600

    functions created

--- lesson4/build/js/main.js
+++ lesson4/build/js/main.js
@@ -2,7 +2,7 @@
 // Literal types
 let myName;
 let userName;
-userName = 'Amy';
+userName = "Amy";
 // Functions
 const add = (num1, num2) => {
     return num1 + num2;
@@ -10,5 +10,9 @@ const add = (num1, num2) => {
 const messagePrint = (message) => {
     console.log(message);
 };
+const subTract = function (num1, num2) {
+    return num1 - num2;
+};
 console.log(add(5, 6));
 messagePrint("This is TypeScript Function");
+console.log(subTract(11, 6));
--- lesson4/src/main.ts
+++ lesson4/src/main.ts
@@ -1,31 +1,36 @@
-// Type Aliases 
-type stringOrNumber = string | number
+// Type Aliases
+type stringOrNumber = string | number;
 
-type stringOrNumberArray = (string | number)[]
+type stringOrNumberArray = (string | number)[];
 
 type Guitarist = {
-    name?: string,
-    active: boolean,
-    albums: stringOrNumberArray
-}
+  name?: string;
+  active: boolean;
+  albums: stringOrNumberArray;
+};
 
-type UserId = stringOrNumber
+type UserId = stringOrNumber;
 
 // Literal types
-let myName: 'Dave'
+let myName: "Dave";
 
-let userName: 'Dave' | 'John' | 'Amy'
-userName = 'Amy'
+let userName: "Dave" | "John" | "Amy";
+userName = "Amy";
 
 // Functions
 
-const add =(num1:number,num2:number):number=>{
-    return num1+num2
-}
+const add = (num1: number, num2: number): number => {
+  return num1 + num2;
+};
 
-const messagePrint=(message:any):void=>{
-    console.log(message);
-}
+const messagePrint = (message: any): void => {
+  console.log(message);
+};
 
-console.log(add(5,6));
+const subTract = function (num1: number, num2: number): number {
+  return num1 - num2;
+};
+
+console.log(add(5, 6));
 messagePrint("This is TypeScript Function");
+console.log(subTract(11,6));
