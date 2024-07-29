76c5e6da7a82667259a9ef7ddce15726cf85c8f3
TonmoyZohani <tonmoyzohani@gmail.com>
2024-07-29 21:48:26 +0600

    all consoles commented

--- lesson4/src/main.ts
+++ lesson4/src/main.ts
@@ -60,10 +60,10 @@ const calculateSum = (...nums: number[]): number => {
   return nums.reduce((sum, num) => sum + num, 0);
 };
 
-console.log(add(5, 6));
-messagePrint("This is TypeScript Function");
-console.log(subTract(11, 6));
-console.log(multiply(3, 8));
-console.log(sumAll(7, 9, 8));
-console.log(addAll(5, 9));
-console.log(calculateSum(1, 2, 3, 4));
+// console.log(add(5, 6));
+// messagePrint("This is TypeScript Function");
+// console.log(subTract(11, 6));
+// console.log(multiply(3, 8));
+// console.log(sumAll(7, 9, 8));
+// console.log(addAll(5, 9));
+// console.log(calculateSum(1, 2, 3, 4));
