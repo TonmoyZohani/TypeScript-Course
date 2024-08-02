d0f7543b9ab098291417b01a9ae90e8033a1f59a
TonmoyZohani <tonmoyzohani@gmail.com>
2024-08-02 22:30:38 +0600

    type revision fixed

--- lesson1/src/main.ts
+++ lesson1/src/main.ts
@@ -5,8 +5,16 @@ console.log(username);
 // let b: string = "6";
 // let c: number = 2;
 
-let a: number =12;
-let b
-
-console.log(a / c);
+// console.log(a / c);
 // console.log(username)
+
+const message:string = "hello world!";
+let a:number = 15;
+let b:number =5;
+let c:any = 3;
+let d:string = "15";
+
+console.log(message);
+console.log(a/b);
+console.log(a/c);
+// console.log(a/d);
