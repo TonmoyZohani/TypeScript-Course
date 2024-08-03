5e6ae5b0a871876912f247324e59e5bc777c19c3
TonmoyZohani <tonmoyzohani@gmail.com>
2024-08-03 22:20:58 +0600

    type revision fixed

--- lesson3/src/main.ts
+++ lesson3/src/main.ts
@@ -75,12 +75,12 @@ console.log(jp);
 // Enums
 // "Unlike most TypeScript features, Enums are not a type-level addition to JavaScript but something added to the language and runtime."
 
-// enum Grade {
-//   U = 1,
-//   D,
-//   C,
-//   B,
-//   A,
-// }
+enum Grade {
+  U = 1,
+  D,
+  C,
+  B,
+  A,
+}
 
 console.log(Grade.U);
