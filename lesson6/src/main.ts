8ca1568341190e30712155334f0dd74f5047ac1c
Khaled Zohani Tonmoy <94390552+TonmoyZohani@users.noreply.github.com>
2024-08-18 21:54:56 +0600

    added

--- lesson6/src/main.ts
+++ lesson6/src/main.ts
@@ -3,21 +3,36 @@ class Person {
     private firstName: string;
     private lastName: string;
     private age: number;
-
+  
     // Constructor
     constructor(firstName: string, lastName: string, age: number) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
     }
-
+  
     // Public method to get full name
     public getFullName(): string {
-        return `${this.firstName} ${this.lastName}`;
+      return `${this.firstName} ${this.lastName}`;
     }
-
+  
     // Public method to get age
-    public getAge(): string {
-        return this.age
+    public getAge(): number {
+      return this.age;
     }
-}
+  
+    // Method to increment age
+    public increaseAge(): void {
+      this.age++;
+    }
+  }
+  
+  const person = new Person("Tonmoy", "Zohani", 30);
+  
+  console.log(person.getFullName());
+  console.log(person.getAge());
+  
+  person.increaseAge();
+  console.log(person.getAge());
+  
+  
